f593f411fc93255910e0931dbf2dd6af5c8c9184
prashankln <mwendaslot@gmail.com>
2019-03-11 08:46:14 +0530

    counter

--- app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
+++ app/src/main/java/lk/ac/kln/countdowntimer/MainActivity.java
@@ -12,6 +12,7 @@ public class MainActivity extends AppCompatActivity {
     private boolean running;
     private boolean wasRunning;
     private static final String CURRENT_COUNTER= "counter";
+    
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
